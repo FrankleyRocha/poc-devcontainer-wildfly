e7ceb67d14ec8e332c49b1f616ec39894bd7eb23
Frankley Francalino da Rocha <rocha112358@gmail.com>
2025-04-08 23:04:38 -0300

    ok

--- workspace/hello-wildfly/src/main/java/io/github/frankleyrocha/hello_wildfly/api/HelloResource.java
+++ workspace/hello-wildfly/src/main/java/io/github/frankleyrocha/hello_wildfly/api/HelloResource.java
@@ -11,7 +11,7 @@ public class HelloResource {
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
-        return "Olá wilfly!";
+        return "Olá wilfly! ABACAXI!";
     }
 
 }
